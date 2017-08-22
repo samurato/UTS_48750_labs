3073ec831d40e9a989164758bf9ad97e78f5cc3d
Patrick Miziewicz <patrick@keylog.com.au>
2017-08-22 16:30:48 +1000

    Added base file and Line class

---
+++ MMN model/main.m
@@ -0,0 +1,22 @@
+% M/M/N simulation
+% Patrick Miziewicz & Zach Newton
+
+%constants
+max_time = 30.0;
+call_duration = 12;
+trunk_count = 30;
+observed_call_count = 50;
+number_of_ticks = 0;
+
+%variables
+average_lines_in_use = 0.0;
+grade_of_service = 0.0;
+simulation_time = 0.0;
+
+% Initialise Line array
+all_comm_lines(1, trunk_count) = ObjectConstructor(); %TODO
+
+for i = 1:number_of_ticks
+    
+end
+
