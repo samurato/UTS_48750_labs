7e1050b1ea2b97b270643271f2b99a44ad4789d8
Patrick Miziewicz <patrick@keylog.com.au>
2017-09-02 22:19:47 +1000

    Added logic to record the amount of people in a call and the amount of dropped calls(if any)

--- MMN model/main.m
+++ MMN model/main.m
@@ -12,15 +12,37 @@ number_of_ticks = 0;
 average_lines_in_use = 0.0;
 grade_of_service = 0.0;
 simulation_time = 60;
+qeueue_length = 0;
 all_trunks(1:trunk_count) = Trunk(avg_call_duration);
 call_probability = input('Enter the call probability (0.00 - 1.00): ');
 results(1:simulation_time) = 0;
-dropped_call(1:simulation_time) = 0;
+dropped_calls(1:simulation_time) = 0;
 
 for i = 1:number_of_ticks
+    
+    qeueue_length = 0;% calculate amount of people to put in calls this tick
+                      % do not override this variable. Values carry over
+                      % from tick to tick
+    people_in_calls = 0;
     for ii = 1:trunk_count
         all_trunks(ii).tick();
+        if all_trunks(ii).is_in_call == true
+            people_in_calls = people_in_calls + 1;
+            % Gen rand num and check against probability of hanging up
+            % hang up if true
+            if true
+                all_trunks(ii).endCall();
+            end
+        elseif qeueue_length > 0
+            % Gen rand num and check against probability of picking up
+            if true
+               all_trunks(ii).startCall(); 
+            end
+            qeueue_length = qeueue_length - 1;
+        end
     end
+    results(i) = people_in_calls;
+    dropped_calls(i) = qeueue_length;
 end
 
 time(1:simulation_time) = 0;
